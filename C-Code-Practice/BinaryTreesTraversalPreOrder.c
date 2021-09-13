554d177fc886729bd65b6427db40693abfb1e15f
Prajwalmj <mjprajwal2014@gmail.com>
2021-09-12 19:06:35 -0500

    First code

---
+++ C-Code-Practice/BinaryTreesTraversalPreOrder.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct BinaryTreeNode{
+    int data;
+    struct BinaryTreeNode *left;
+    struct BinaryTreeNode *right;
+};
+
+int main(void){
+    
+    return 0;
+}
